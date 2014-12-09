a0c0e31b86f16b9e018d76e41cfb6b6b7255dac6
Bill Rawlinson <bill.rawlinson@sbcs.com>
2014-12-09 10:05:07 -0500

    Add ability to optionally select a specific test to run.

--- MxUnitRunnerBrowserCommand.py
+++ MxUnitRunnerBrowserCommand.py
@@ -18,11 +18,23 @@ class MxUnitRunnerBrowserCommand(sublime_plugin.TextCommand):
 	def getProjectSettings(self, view):
 		return view.settings().get("mxunit-runner")
 
-	def convertToTestURL(self, fileToOpen, basePath, baseUrl, additionalUrlParameters):
-		return re.sub(r"\\", "/", fileToOpen.replace(basePath, baseUrl)) + "?method=runtestremote&output=html&" + additionalUrlParameters
+	def convertToTestURL(self, fileToOpen, basePath, baseUrl, testMethod, additionalUrlParameters):
+		urlPath = fileToOpen.replace(basePath, baseUrl)
+		urlPath = re.sub(r"\\", "/", urlPath) + "?method=runtestremote&output=html"
+
+		#if a user selected a specific test method to run we should just run that one test instead of the entire file.
+		tmLen = len(testMethod)
+		if tmLen > 0:
+			urlPath += "&testMethod=" + testMethod
+		urlPath += additionalUrlParameters
+		return urlPath
 
 	def run(self, edit):
 		print("MXUnit Runner plugin v{0}, Python {1}".format(PLUGIN_VERSION, self._pythonVersion))
+
+		selectedRegion = self.view.sel()
+		testMethod = self.view.substr(selectedRegion[0])
+
 
 		projectSettings = self.getProjectSettings(self.view)
 		fileToOpen = self.view.file_name()
@@ -32,7 +44,7 @@ class MxUnitRunnerBrowserCommand(sublime_plugin.TextCommand):
 			return
 
 		additionalUrlParameters = projectSettings["additionalUrlParameters"] if "additionalUrlParameters" in projectSettings else ""
-		urlPath = self.convertToTestURL(fileToOpen, projectSettings["testsDirectory"], projectSettings["urlPrefix"], additionalUrlParameters)
+		urlPath = self.convertToTestURL(fileToOpen, projectSettings["testsDirectory"], projectSettings["urlPrefix"], testMethod, additionalUrlParameters)
 
 		#
 		# Run tests in default web browser

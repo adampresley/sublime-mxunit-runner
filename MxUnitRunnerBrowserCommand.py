450a6e1d4d0c9f8efc76e39da9ff90feaee55491
Bill Rawlinson <bill.rawlinson@sbcs.com>
2014-12-09 13:54:10 -0500

    merged cleaned up code; as I had started this branch before the cleanup obviously

--- MxUnitRunnerBrowserCommand.py
+++ MxUnitRunnerBrowserCommand.py
@@ -19,22 +19,15 @@ class MxUnitRunnerBrowserCommand(sublime_plugin.TextCommand):
 		return view.settings().get("mxunit-runner")
 
 	def convertToTestURL(self, fileToOpen, basePath, baseUrl, testMethod, additionalUrlParameters):
-		urlPath = fileToOpen.replace(basePath, baseUrl)
-		urlPath = re.sub(r"\\", "/", urlPath) + "?method=runtestremote&output=html"
+		urlPath = re.sub(r"\\", "/", fileToOpen.replace(basePath, baseUrl)) + "?method=runtestremote&output=html" + additionalUrlParameters
 
-		#if a user selected a specific test method to run we should just run that one test instead of the entire file.
-		tmLen = len(testMethod)
-		if tmLen > 0:
+		if len(testMethod) > 0:
 			urlPath += "&testMethod=" + testMethod
-		urlPath += additionalUrlParameters
+
 		return urlPath
 
 	def run(self, edit):
 		print("MXUnit Runner plugin v{0}, Python {1}".format(PLUGIN_VERSION, self._pythonVersion))
-
-		selectedRegion = self.view.sel()
-		testMethod = self.view.substr(selectedRegion[0])
-
 
 		projectSettings = self.getProjectSettings(self.view)
 		fileToOpen = self.view.file_name()
@@ -44,6 +37,11 @@ class MxUnitRunnerBrowserCommand(sublime_plugin.TextCommand):
 			return
 
 		additionalUrlParameters = projectSettings["additionalUrlParameters"] if "additionalUrlParameters" in projectSettings else ""
+
+		# determine if the user has selected a specific method to run instead of running all tests in the file
+		selectedRegion = self.view.sel()
+		testMethod = self.view.substr(selectedRegion[0])
+
 		urlPath = self.convertToTestURL(fileToOpen, projectSettings["testsDirectory"], projectSettings["urlPrefix"], testMethod, additionalUrlParameters)
 
 		#

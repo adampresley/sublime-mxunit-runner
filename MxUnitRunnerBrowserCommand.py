5b7814fc58cbf9cbf1a4ab1d92b5dd8dc3d5a978
Bill Rawlinson <bill.rawlinson@sbcs.com>
2014-12-09 11:00:34 -0500

    moved logic to get test method lower in function
    as the entire process will abort if there is no project settings file I moved the logic for getting the test method until after
    the potential abort so it would be more efficient.

--- MxUnitRunnerBrowserCommand.py
+++ MxUnitRunnerBrowserCommand.py
@@ -32,10 +32,6 @@ class MxUnitRunnerBrowserCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		print("MXUnit Runner plugin v{0}, Python {1}".format(PLUGIN_VERSION, self._pythonVersion))
 
-		selectedRegion = self.view.sel()
-		testMethod = self.view.substr(selectedRegion[0])
-
-
 		projectSettings = self.getProjectSettings(self.view)
 		fileToOpen = self.view.file_name()
 
@@ -44,6 +40,11 @@ class MxUnitRunnerBrowserCommand(sublime_plugin.TextCommand):
 			return
 
 		additionalUrlParameters = projectSettings["additionalUrlParameters"] if "additionalUrlParameters" in projectSettings else ""
+
+		# determine if the user has selected a specific method to run instead of running all tests in the file
+		selectedRegion = self.view.sel()
+		testMethod = self.view.substr(selectedRegion[0])
+
 		urlPath = self.convertToTestURL(fileToOpen, projectSettings["testsDirectory"], projectSettings["urlPrefix"], testMethod, additionalUrlParameters)
 
 		#
